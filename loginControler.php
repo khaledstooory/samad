c4c9e61952d1b6041131bd1cc426d896eeb08583
khaledstooory <58520128+khaledstooory@users.noreply.github.com>
2020-02-23 18:21:56 +0200

    Update loginControler.php

--- loginControler.php
+++ loginControler.php
@@ -7,45 +7,17 @@ die();
 }
 ?>
 <?php
-
-include './include/init.php';
-include './include/autoLoader.php';
-if($_POST or @$_GET)
+if(isset($_POST['login']))
 {
-    if(isset($_POST['Login']) and $_POST['Login']=="Login")
-    {
-        
-        
-  
-        try 
-        {
-            $valid=new validator();
-            $rules=array("username"=>"checkString");
-            if($valid->validate($_POST, $rules)){
-                $username=$_POST['username'];
-            $password=$_POST['password'];
-            }
-            $login=new login($username,$password);
-            if($mydata=$login->Data())
-            {
-                session_start();
-                $_SESSION['username']=$username;
-                $_SESSION['name']=$mydata['name'];
-               header('Location:./index.php');
-            }
-            else {
-                include './veiws/v_login.php';
-                echo '<script>alert("خطأ في اسم المستخدم او كلمة المرور");</script>';
-            }
-        } 
-        catch (Exception $ex) {
-            echo $ex->getMessage();
-        }
-    }
-   
+$username=$_POST["username"];
+$password=$_POST["password"];
+if($username=="admin" && $password="123")
+ header("Location: index.php");
+else
+echo "Please Try again incorrect password or username";
 }
- else {
-    include './veiws/v_login.php';
+if(isset($_POST['back']))
+{
+header("Location: index.php");
 }
 ?>
-
