00bb9436a30a106af129487176baa04aac26f692
khaledstooory <58520128+khaledstooory@users.noreply.github.com>
2020-02-23 18:27:34 +0200

    Update loginControler.php

--- loginControler.php
+++ loginControler.php
@@ -2,7 +2,7 @@
 @session_start();
 if(isset($_SESSION['username']))
 {
-header('Location:index.php'); 
+header( 'Location:index.php' ); 
 die();
 }
 ?>
@@ -12,12 +12,12 @@ if(isset($_POST['login']))
 $username=$_POST["username"];
 $password=$_POST["password"];
 if($username=="admin" && $password="123")
- header("Location: index.php");
+ header( 'Location: index.php' );
 else
 echo "Please Try again incorrect password or username";
 }
 if(isset($_POST['back']))
 {
-header("Location: index.php");
+header( 'Location: index.php' );
 }
 ?>
